6fb612aee402e9a198fd5fdfad7a8d8ae2ad96ba
bradleyham <bradleyjamesham@gmail.com>
2020-06-17 14:02:04 +1200

    Updates

---
+++ precache-manifest.7f3146ec008d69b8b2e33524f5f8503b.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8cf7379439f4435aea7f96cd5cf5512c",
+    "url": "/restaurant/index.html"
+  },
+  {
+    "revision": "07eb6c4d815ab8ad5361",
+    "url": "/restaurant/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "8738e67e85eb0041343a",
+    "url": "/restaurant/static/css/main.a488a356.chunk.css"
+  },
+  {
+    "revision": "07eb6c4d815ab8ad5361",
+    "url": "/restaurant/static/js/2.1ee3e1f0.chunk.js"
+  },
+  {
+    "revision": "723894b771c1fe011b0df55ec646ea2d",
+    "url": "/restaurant/static/js/2.1ee3e1f0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8738e67e85eb0041343a",
+    "url": "/restaurant/static/js/main.5285f6cb.chunk.js"
+  },
+  {
+    "revision": "b09720dba522e0616906",
+    "url": "/restaurant/static/js/runtime-main.af041a5e.js"
+  },
+  {
+    "revision": "2fc61a366160db0724775da9b456768e",
+    "url": "/restaurant/static/media/beef-mobile@2x.2fc61a36.jpg"
+  },
+  {
+    "revision": "71d394d0f8f7544d9379bd685319e573",
+    "url": "/restaurant/static/media/chocolate-mobile@2x.71d394d0.jpg"
+  },
+  {
+    "revision": "2dfa948e2d2120d2691f8c77f2f12d05",
+    "url": "/restaurant/static/media/enjoyable-place-desktop@2x.2dfa948e.jpg"
+  },
+  {
+    "revision": "38e2cfbc2b1612884942257a41aaa943",
+    "url": "/restaurant/static/media/enjoyable-place-mobile@2x.38e2cfbc.jpg"
+  },
+  {
+    "revision": "36532372987f39ab653e3acd0f1d974a",
+    "url": "/restaurant/static/media/enjoyable-place-tablet@2x.36532372.jpg"
+  },
+  {
+    "revision": "da6024673bea269f0a01f13c2987a0f5",
+    "url": "/restaurant/static/media/family-gathering-mobile@2x.da602467.jpg"
+  },
+  {
+    "revision": "d69ea4f8353adc5effcc0ec0e2541aee",
+    "url": "/restaurant/static/media/hero-bg-desktop@2x.d69ea4f8.jpg"
+  },
+  {
+    "revision": "f8e1664f5798769f36dc57ce000cef41",
+    "url": "/restaurant/static/media/hero-bg-mobile@2x.f8e1664f.jpg"
+  },
+  {
+    "revision": "b5bbd83a5b30addfcc89c3cf49d26c6e",
+    "url": "/restaurant/static/media/hero-bg-tablet@2x.b5bbd83a.jpg"
+  },
+  {
+    "revision": "9ee235724df584d44cd16efcb0d0a454",
+    "url": "/restaurant/static/media/locally-sourced-desktop@2x.9ee23572.jpg"
+  },
+  {
+    "revision": "8c0135c080eca620327740f425215391",
+    "url": "/restaurant/static/media/locally-sourced-mobile@2x.8c0135c0.jpg"
+  },
+  {
+    "revision": "6b39764dd020d7e41c9068371c8c5cf8",
+    "url": "/restaurant/static/media/locally-sourced-tablet@2x.6b39764d.jpg"
+  },
+  {
+    "revision": "77989ac9c8c298c7c7b164f724c3dcb9",
+    "url": "/restaurant/static/media/pattern-lines.77989ac9.svg"
+  },
+  {
+    "revision": "55d123bffefc7ceb476ea74d8eeaa328",
+    "url": "/restaurant/static/media/ready-bg-desktop@2x.55d123bf.jpg"
+  },
+  {
+    "revision": "d480732f1ed0813452bf24d0622b2a0d",
+    "url": "/restaurant/static/media/ready-bg-mobile.d480732f.jpg"
+  },
+  {
+    "revision": "ded477109fbcb6a9a2e0f0563d7f0e61",
+    "url": "/restaurant/static/media/ready-bg-tablet.ded47710.jpg"
+  },
+  {
+    "revision": "d31c4835512dc92ffbd2f004466fc01d",
+    "url": "/restaurant/static/media/salmon-mobile@2x.d31c4835.jpg"
+  },
+  {
+    "revision": "9c157134af789f2116b26430be267e40",
+    "url": "/restaurant/static/media/social-events-desktop@2x.9c157134.jpg"
+  },
+  {
+    "revision": "3a427a80553bf94f63a0bc98d1b40a42",
+    "url": "/restaurant/static/media/special-events-desktop@2x.3a427a80.jpg"
+  }
+]);
